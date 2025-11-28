f3d33612f696e603d684ae6884ca0599fd1a8898
CmloPvl <camiloprg2@gmail.com>
2025-11-27 23:38:31 -0300

    fix header image for github pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import "../components/Header.css";
 import headerImg from "../assets/imgs/Header.jpg";
+
 const headerStyle = {
   backgroundImage: `linear-gradient(
       rgba(0, 0, 0, 0.55),
@@ -7,19 +8,17 @@ const headerStyle = {
     ), url(${headerImg})`,
   backgroundSize: "cover",
   backgroundPosition: "top",
-  padding: "100px 200px",
+  padding: "120px 20px",
   color: "white",
   textShadow: "0 0 5px rgba(0, 0, 0, 0.9)",
 };
 
-
-
 const Header = () => {
   return (
     <header className="header text-center" style={headerStyle}>
-      <h1>Pizzería Mamma Mia!</h1>
-      <p>¡Tenemos las mejores pizzas que podrás encontrar!</p>
-      <hr />
+      <h1 className="fw-bold">Pizzería Mamma Mia!</h1>
+      <p className="fs-5">¡Tenemos las mejores pizzas que podrás encontrar!</p>
+      <hr className="mt-4 border-light w-50" />
     </header>
   );
 };
