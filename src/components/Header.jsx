72111755f512f3367deb002d5647605594124f1e
CmloPvl <camiloprg2@gmail.com>
2025-11-27 23:35:53 -0300

    fix imagen header para github pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,22 @@
 import "../components/Header.css";
+import headerImg from "../assets/imgs/Header.jpg";
+const headerStyle = {
+  backgroundImage: `linear-gradient(
+      rgba(0, 0, 0, 0.55),
+      rgba(0, 0, 0, 0.55)
+    ), url(${headerImg})`,
+  backgroundSize: "cover",
+  backgroundPosition: "top",
+  padding: "100px 200px",
+  color: "white",
+  textShadow: "0 0 5px rgba(0, 0, 0, 0.9)",
+};
+
+
 
 const Header = () => {
   return (
-    <header className="header text-center">
+    <header className="header text-center" style={headerStyle}>
       <h1>Pizzería Mamma Mia!</h1>
       <p>¡Tenemos las mejores pizzas que podrás encontrar!</p>
       <hr />
