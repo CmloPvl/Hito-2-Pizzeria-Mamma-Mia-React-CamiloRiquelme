32c831f6ace57610e493cb100ecadf73752813a5
CmloPvl <camiloprg2@gmail.com>
2025-11-27 23:27:32 -0300

    nuevo build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Hito-1-PizzeriaMammaMia-React-CamiloRiquelme/",
+  base: process.env.NODE_ENV === "production"
+    ? "/Hito-1-PizzeriaMammaMia-React-CamiloRiquelme/"
+    : "/",
 })
