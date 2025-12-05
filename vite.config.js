faec58ecd0662b2bdac0bbd0cfb5cb74a9b96bc6
CmloPvl <camiloprg2@gmail.com>
2025-12-04 22:30:13 -0300

    Arreglar base de Vite y homepage para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Hito-2-PizzeriaMammaMia-React-CamiloRiquelme/",  // 👈 importantísimo
+  base: "/Hito-2-Pizzeria-Mamma-Mia-React-CamiloRiquelme/",  
 })
